6f77f1f2e521507ab299fcf7e255da3ac016a9e7
João <konestiqui@gmail.com>
2022-07-11 11:33:13 -0300

    apenas isso

---
+++ aulaJsPhp/paginas/listProduto.php
@@ -0,0 +1,19 @@
+<?php
+    include "../include/mysql.php";
+
+    $sql = $pdo ->prepare("SELECT * FROM produto");
+    if($sql->execute()){
+        $info = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($info as $key=>$values){
+            echo 'Codigo: '.$values['codProduto'].'<br>';
+            echo 'Descrição: '.$values['descProduto'].'<br>';
+            echo 'Valor: '.$values['valorProduto'].'<br>';
+
+            $imagem = $values['imagem'];
+            echo '<img src="data:image/jpg;charset=utf8;base64,'.base64_encode($imagem).'"/><br>';
+            echo'<hr>';
+        }
+
+    }
+?>
